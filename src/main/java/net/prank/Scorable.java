258815119282038367c6c01fad9e2ea2a8878797
David Millett <dbmillett@gmail.com>
2013-05-11 20:27:55 -0500

    adding apache to copyright notice to each source file

--- src/main/java/net/prank/Scorable.java
+++ src/main/java/net/prank/Scorable.java
@@ -4,6 +4,22 @@ package net.prank;
  * Use if you are ok updating your scorable object so that
  * you can use a generic comparator on it. Otherwise, just
  * update your model and define a more specific comparator.
+ *
+ *
+ * @author dmillett
+ *
+ * Copyright 2012 David Millett
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 public interface Scorable {
     /** Contains any ScoreCard's and their scores */
--- src/main/java/net/prank/ScoreCard.java
+++ src/main/java/net/prank/ScoreCard.java
@@ -2,6 +2,23 @@ package net.prank;
 
 /**
  * A thread pool exists for each configured (spring) ScoreCard where it will examine all
+ * the relevant ScoreCards and evaluate them. Implement a separate ScoreCard for ranking
+ * for any/all discrete ranking topics.
+ *
+ * @author dmillett
+ *
+ * Copyright 2012 David Millett
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 public interface ScoreCard<T> {
     /**
